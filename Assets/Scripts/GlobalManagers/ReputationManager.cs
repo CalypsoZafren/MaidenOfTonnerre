d4b291a7296e20238dffc6f929ac0d852d1e2ac9
Callie Zafren <calliezafren@gmail.com>
2026-07-03 16:57:04 +0100

    Updates and Beginning of Systems
    - Basic Reputation System
    - UI Implementatin Beginning

---
+++ Assets/Scripts/GlobalManagers/ReputationManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a3efc95b575ba5fe70b267d754e12607fdf7c8309a475713dc126bea5fc3155
+size 2177
---
+++ Assets/Scripts/GlobalManagers/UIManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:97ab795a807224be4d41477930298088ad280534fba039454bbb7c44969a8e3f
+size 2052
